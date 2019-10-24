31482e1ab94297238000530a9882e393d9e41b32
Jiadan <jiadanwang@gmail.com>
2019-10-24 02:33:45 +0000

    add posts page

--- express_server.js
+++ express_server.js
@@ -4,6 +4,12 @@ const PORT = 3000;
 
 
 app.set("view engine", "ejs");
+
+postDatabase = {
+  "jiadanwang@gmail.com" : {title : "Express", description : "I'm using Express"},
+  "wang@gmail.com" : {title : "Node", description : " "},
+  "haha@gmail.com" : {title : "js", description : "very useful"}
+}
 app.listen(PORT,() => {
   console.log(`listening on port ${PORT}`)
 })
@@ -15,3 +21,15 @@ app.get("/",(req,res) => {
 app.get("/posts/submit", (req, res) => {
   res.render("create");
 })
+
+app.get("/posts", (req, res) => {
+  let templateVars = {posts : postDatabase};
+  res.render("index", templateVars)
+})
+
+
+
+
+
+
+
